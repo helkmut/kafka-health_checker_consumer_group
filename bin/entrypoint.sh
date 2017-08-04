a3b1cea5bba39894f33df61f93234362152df759
helkmut <root@ilegra-note-gprestes.athome.org>
2017-08-04 16:14:50 -0300

    v0.2

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sed -i 's/$burrow_host/'"$1"'/g' kafka-health_checker_consumer_group/etc/attributesmonitoring.conf
-exec "$@"
+#exec "$@"
 
 sed -i 's/$kafka_cluster/'"$2"'/g' kafka-health_checker_consumer_group/etc/attributesmonitoring.conf
-exec "$@"
+#exec "$@"
 
